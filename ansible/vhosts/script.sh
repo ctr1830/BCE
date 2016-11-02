88dafa569d38305e89c427184923d0438f8d62cc
Borja Minguela <bminguela001@ikasle.ehu.eus>
2016-11-02 13:18:12 +0100

    Solucionado un error que impedia la ejecucion del script

--- ansible/vhosts/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir /home/ikasle/vhost
-mv /home/ikasle/vhost* /home/ikasle/vhost
-docker stop nginx
-docker rm nginx
-docker run --name nginx -d -p 8080:80 -v /home/ikasle/vhost:/etc/ngnx/conf.d nginx
-docker exec -ti nginx /etc/init.d/nginx reload
